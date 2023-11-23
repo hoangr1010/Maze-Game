78ea30c9d3c8ba51537d67beefcb627f659c364d
JoeFoote <93680046+JoeFoote@users.noreply.github.com>
2023-11-23 00:48:02 -0700

    Reverted main.c

--- src/main.c
+++ src/main.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include "game.h"
+#include "levels.h"
 
 int main(void){
 
+int main(void) {
     initscr();
     start_color();  // Enable colour support
     cbreak();   // Disable line buffering
-    raw();
-    keypad(stdscr, TRUE);   // Enable special keys
-    noecho();   // Do not display input characters
-
     /* Display splash screen */
     splash_screen();
 
     getchar();
+    // Create a window
+    WINDOW *mainWin = newwin(30, 30, 0, 0);
+    box(mainWin,0,0);
+    refresh();
+
+    // Game start with levels
+    level(mainWin, 1);
+
+    level(mainWin, 2);
+
+    level(mainWin, 3);
+
+    getch();
+
+    delwin(mainWin);
     endwin();
 
     return 0;
-}
