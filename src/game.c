eb2be57207ad8218dc316997dc944a1806a5b2db
JoeFoote <93680046+JoeFoote@users.noreply.github.com>
2023-11-23 00:46:55 -0700

    Reverted game.c

--- src/game.c
+++ src/game.c
@@ -1,31 +1,62 @@
 #include <ncurses.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "levels.h"
 #include "game.h"
 
+void readMazeFromFile(char maze[MAX_HEIGHT][MAX_WIDTH], int *rows, int *cols, const char *filename) {
+
+	char filepath[] = "/data/";
+	strcat(filepath, filename); 
+	char filepath[] = "./data/";
+	strcat(filepath, filename);
+
+	FILE *file = fopen(filepath, "r");
+
+	if (file == NULL) {
+      		fprintf(stderr, "\n");
+		endwin();
+		exit(EXIT_FAILURE);
+	}
+
+	*rows = *cols = 0;
+	int c;
+	while ((c=fgetc(file)) != EOF && *rows < MAX_HEIGHT) {
+@@ -27,22 +35,23 @@ void readMazeFromFile(char maze[MAX_HEIGHT][MAX_WIDTH], int *rows, int *cols, co
+
+void drawMaze(WINDOW *win, char maze[MAX_HEIGHT][MAX_WIDTH], int rows, int cols) {
+	wclear(win);
+
+	printw("%c", maze[rows][cols]);
+	for (int i=0;i<rows;i++) {
+		for (int j=0; i<cols; j++) {
+			wprintw(win, "%c", maze[i][j]);
+			wprintw(win,"%c", maze[i][j]);
+		}
+		wprintw(win,"\n");
+	}
+
+	wrefresh(win);
+}
+
 void splash_screen(){
     
+
     if (has_colors()){
         init_pair(1, COLOR_GREEN, COLOR_BLACK);
         attron(COLOR_PAIR(1));
         printw("___  ___               ______\n");            
+        printw("___  ___               ______\n");
         printw("|  \\/  |               | ___ \\\n");
         printw("| .  . | __ _ _______  | |_/ /   _ _ __  \n");
         printw("| |\\/| |/ _` |_  / _ \\ |    / | | | '_ \\\n");
-        printw("| |  | | (_| |/ /  __/ | |\\ \\ |_| | | | |\n");
-        printw("\\_|  |_/\\__,_/___\\___| \\_| \\_\\__,_|_| |_|\n");
-        attroff(COLOR_PAIR(1));
-        printw("\nEscape the maze.\n");
+@@ -53,6 +62,9 @@ void splash_screen(){
         printw("Arrow keys to move, SPACE to attack and Q to use items");
         printw("\npress any key to continue.....");
         refresh();
+
+	getch();
+	clear();
         /*
-        while((ch = getch()) == err()){
-            printw("\npress any key to continue...");
-            refresh();
-        }
-        */
-    }
-    return;
-}
+	int ch;
